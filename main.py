8764fdbbb49ce7f1bb2f298a80db47a13cec3608
anna <qwykeblu@gmail.com>
2022-11-06 15:15:49 +0200

    commented class LoginPage in main.py

--- main.py
+++ main.py
@@ -21,12 +21,12 @@ def logintest():
     login_form.login()
     driver.quit()
 
-class LoginPage(page):
-    def input_username(self, USERNAME):
-        self.find_element(*Login.username).send_keys(USERNAME)
-
-    def input_password(self, PASSWORD):
-        self.find_element(*Login.password).send_keys(PASSWORD)
+# class LoginPage(page):
+#     def input_username(self, USERNAME):
+#         self.find_element(*Login.username).send_keys(USERNAME)
+#
+#     def input_password(self, PASSWORD):
+#         self.find_element(*Login.password).send_keys(PASSWORD)
 
 
 if __name__ == "__main__":
